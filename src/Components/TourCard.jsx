6e80516897388459171016eacfd59c5f0a67c3f4
mrohling13 <malachi.rohling@yahoo.com>
2025-04-20 15:50:07 -0400

    Task 3: Render Tour Cards

---
+++ src/Components/TourCard.jsx
@@ -0,0 +1,44 @@
+// Task 3: Render Tour Cards
+
+
+
+
+import React, { useState } from "react";
+
+
+const TourCard = ({ id, name, info, image, price, onRemove }) => {
+  
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="tour-card">
+      
+      <img src={image} alt={name} className="tour-img" />
+      
+      <div className="tour-info">
+        <div className="tour-header">
+          
+          <h2>{name}</h2>
+          <h3 className="tour-price">${price}</h3>
+        </div>
+        
+        
+        <p>
+          {readMore ? info : `${info.substring(0, 150)}...`}
+          
+          <button className="read-more" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
+        
+        
+        <button className="not-interested" onClick={() => onRemove(id)}>
+          Not Interested
+        </button>
+      </div>
+    </article>
+  );
+}
+
+
+export default TourCard;

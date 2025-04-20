feca1e0ec611fc44ee2a36144530bb8b78a0327d
mrohling13 <malachi.rohling@yahoo.com>
2025-04-20 15:48:31 -0400

    Task 2: Create Dropdown Filter

---
+++ src/Components/DestinationSelector.jsx
@@ -0,0 +1,40 @@
+// Task 2: Create Dropdown Filter 
+
+
+import React from 'react';
+
+
+const DestinationSelector = ({ tours, selectedTour, setSelectedTour }) => {
+  return (
+    <div className="destination-selector">
+      
+      <label htmlFor="destination">Choose a Destination:</label>
+      
+      
+      <select
+        
+        id="destination"
+        
+        
+        value={selectedTour}
+        
+        
+        onChange={(e) => setSelectedTour(e.target.value)} 
+      >
+        
+        <option value="all">All Destinations</option>
+        
+        
+        {tours.map((tour) => (
+          <option key={tour.id} value={tour.name}>
+            
+            {tour.name} 
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+
+export default DestinationSelector;
